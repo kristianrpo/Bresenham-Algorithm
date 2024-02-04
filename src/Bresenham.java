1be1e889fac259edc39cc9aac44865ccc2270434
Kristian <krestrepoo@eafit.edu.co>
2024-02-03 19:07:32 -0500

    "add documentation"

--- src/Bresenham.java
+++ src/Bresenham.java
@@ -1,6 +1,7 @@
 /*
  * Este programa es una aplicación gráfica simple en Java que permite al usuario dibujar líneas.
  * Se utiliza el patrón de diseño de escuchador de eventos del mouse para detectar clics y movimientos del ratón.
+ * Se utiliza una variación del algoritmo de bresenham para pintar lineas en los 8 octantes.
  */
 
 import java.awt.Color;
@@ -14,7 +15,7 @@ import javax.swing.JPanel;
 import javax.swing.JFrame;
 
 /**
- * La clase EventoMouse extiende JPanel y implementa MouseListener para manejar eventos del mouse.
+ * La clase Bresenham extiende JPanel y implementa MouseListener para manejar eventos del mouse.
  * Representa un componente gráfico que permite al usuario dibujar líneas.
  */
 public class Bresenham extends JPanel implements MouseListener {
@@ -30,7 +31,7 @@ public class Bresenham extends JPanel implements MouseListener {
 
     /**
      * Método paintComponent que se llama automáticamente para dibujar en el componente.
-     * Dibuja la línea en el panel.
+     * Dibuja los ejes y la línea en el panel.
      */
     @Override
     public void paintComponent(Graphics g) {
@@ -56,41 +57,47 @@ public class Bresenham extends JPanel implements MouseListener {
 
     }
 
-
-
+    /**
+     * Método para dibujar una línea utilizando el algoritmo de Bresenham.
+     * @param g2d      El objeto Graphics2D utilizado para el dibujo.
+     * @param x1       La coordenada x del punto inicial de la línea.
+     * @param y1       La coordenada y del punto inicial de la línea.
+     * @param x2       La coordenada x del punto final de la línea.
+     * @param y2       La coordenada y del punto final de la línea.
+     */
     public void plotLine(Graphics2D g2d,int x1, int y1, int x2, int y2){
-        int dx= Math.abs(x2-x1); // obtengo el valor de que tanto se mueve en x la linea
-        int dy = Math.abs(y2-y1); // obtengo el valor de que tanto se mueve en y la linea
-        int x = x1; // almaceno valores iniciales de x en variable la cual va a cambiar su valor mientras pinta
-        int y= y1; // almaceno valores iniciales de y en variable la cual va a cambiar su valor mientras pinta
-        int incrementDirectionX = x1 < x2 ? 1 : -1; // determino si el x se mueve en el eje positivo o negativo
-        int incrementDirectionY = y1 < y2 ? 1 : -1; // determino si el y se mueve en el eje positivo o negativo
-        int P = 2*dy-dx; // defino la variable de decision
-        int temp = 0; // variable de control para definir cuando se pinto la linea.
-        if ((dy==0)&&(dx==0)){ // Verificar si es solo un pixel, para permitir pintar puntos. Si es una linea, continua al ciclo
+        int dx= Math.abs(x2-x1); // obtengo el valor de que tanto se mueve en x la linea.
+        int dy = Math.abs(y2-y1); // obtengo el valor de que tanto se mueve en y la linea.
+        int x = x1; // almaceno valores iniciales de X en variable la cual va a cambiar su valor mientras pinta.
+        int y= y1; // almaceno valores iniciales de Y en variable la cual va a cambiar su valor mientras pinta.
+        int incrementDirectionX = x1 < x2 ? 1 : -1; // determino si el X se mueve en el eje positivo o negativo.
+        int incrementDirectionY = y1 < y2 ? 1 : -1; // determino si el Y se mueve en el eje positivo o negativo.
+        int P = 2*dy-dx; // defino la variable de decision.
+        int temp = 0; // variable de control para definir cuando se pintó toda la linea.
+        if ((dy==0)&&(dx==0)){ // Verificar si es solo un pixel, para permitir pintar puntos. Si es una linea, continua al ciclo.
             g2d.drawLine(x,y,x,y);
             return;
         }
-        while ((dx < dy || temp != dx) && (dy < dx || temp != dy)) { // mientras que no se haya recorrido toda la distancia a pintar
-            g2d.drawLine(x, y, x, y); // pinto mi punto actual
-            if (dx >= dy) { //verifico si me estoy moviendo mas en x o en y, si es en x
-                x = x + incrementDirectionX; // mi x incrementa constamentemente de a 1
-                if (P < 0) { // si mi variable de decision es menor a 0, escojo el mismo y
+        while ((dx < dy || temp != dx) && (dy < dx || temp != dy)) { // mientras que no se haya recorrido toda la distancia a pintar.
+            g2d.drawLine(x, y, x, y); // pinto mi punto actual.
+            if (dx >= dy) { // verifico si me estoy moviendo mas en x o en y. Si es en X:.
+                x = x + incrementDirectionX; // mi X incrementa constamentemente de a 1.
+                if (P < 0) { // si mi variable de decision es menor a 0, escojo el mismo Yk.
                     P = P + 2 * dy;
-                } else { // de lo contrario, escojo el y+1 || y-1 (dependiendo la direccion donde nos estamos moviendo)
+                } else { // de lo contrario, escojo el y+1 || y-1 (dependiendo la direccion donde nos estamos moviendo).
                     P = P + 2 * dy - 2 * dx;
                     y = y + incrementDirectionY;
                 }
-            } else { // si por el contrario me estoy moviendo mas en y que en x
-                y = y + incrementDirectionY;  // mi y incrementa constantemente de a 1
-                if (P < 0) { //si mi variable de decision es menor a 0 escojo xk
+            } else { // si por el contrario me estoy moviendo mas en y que en X.
+                y = y + incrementDirectionY;  // mi y incrementa constantemente de a 1.
+                if (P < 0) { //si mi variable de decision es menor a 0 escojo Xk.
                     P = P + 2 * dx;
-                } else { // de lo contrario, escojo x+1 || x-1 (dependiendo la direccion donde nos estamos moviendo)
+                } else { // de lo contrario, escojo x+1 || x-1 (dependiendo la direccion donde nos estamos moviendo).
                     P = P + 2 * dx - 2 * dy;
                     x = x + incrementDirectionX;
                 }
             }
-            temp++; // Marco el pixel como recorrido al cambiar mi variable de control.
+            temp++; // marco el pixel como recorrido al cambiar mi variable de control.
         }
     }
 
@@ -100,17 +107,17 @@ public class Bresenham extends JPanel implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        // No se utiliza en este ejemplo
+        // No se utiliza en este ejemplo.
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-        // No se utiliza en este ejemplo
+        // No se utiliza en este ejemplo.
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-        // No se utiliza en este ejemplo
+        // No se utiliza en este ejemplo.
     }
 
     /**
@@ -135,18 +142,18 @@ public class Bresenham extends JPanel implements MouseListener {
     }
 
     /**
-     * Método principal que crea un JFrame y agrega una instancia de EventoMouse a él.
+     * Método principal que crea un JFrame y agrega una instancia de Bresenham a él.
      */
     public static void main(String[] args) {
-        // Crear un nuevo Frame, el cual es la ventana donde añadiremos varios objetos
+        // crear un nuevo Frame, el cual es la ventana donde añadiremos varios objetos.
         JFrame frame = new JFrame("Bresenham");
-        // Configurar que al cerrar el frame pare la ejecución del programa
+        // configurar que al cerrar el frame pare la ejecución del programa.
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Agregar un JPanel que se llama Points, es el contenedor donde tendremos los elementos
+        // agregar un JPanel que se llama Points, es el contenedor donde tendremos los elementos.
         Bresenham ev = new Bresenham();
-        frame.add(ev); // Añadimos el contenedor al frame
-        frame.setSize(500, 500); // Definimos el tamaño del frame
-        frame.setLocationRelativeTo(null); // Ponemos el frame en el centro de la pantalla
-        frame.setVisible(true); // Hacemos visible el frame al usuario
+        frame.add(ev); // añadimos el contenedor al frame.
+        frame.setSize(500, 500); // definimos el tamaño del frame.
+        frame.setLocationRelativeTo(null); // ponemos el frame en el centro de la pantalla.
+        frame.setVisible(true); // hacemos visible el frame al usuario.
     }
 }
